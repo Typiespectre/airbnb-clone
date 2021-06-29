fba17602a703d6e945a6ed1a71ab7b924148f125
Choi GyeongGeon <ckgun@naver.com>
2021-06-30 00:28:21 +0900

    2021.06.29

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
-# from django.shortcuts import render, redirect
 # from django.core.paginator import Paginator, EmptyPage
-from django.views.generic import ListView
+# from django.shortcuts import render, redirect
+# from django.urls import reverse
+# from django.http import Http404
+from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from . import models
 
@@ -41,6 +43,7 @@ def all_rooms(request):
 """
 
 
+# using class based views
 # using ListView abstraction / 참고: ccbv.com
 class HomeView(ListView):
 
@@ -57,3 +60,20 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+"""
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+"""
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
